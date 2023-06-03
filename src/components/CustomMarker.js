42ef1c5282c6f4f53c61910c351d36c5f3d307a2
alicelbh <alice@cogibulle.com>
2023-06-03 10:41:40 +0200

    DB link

--- src/components/CustomMarker.js
+++ src/components/CustomMarker.js
@@ -1,6 +1,7 @@
-import { useState, useRef, useMemo, useCallback } from "react";
+import { useState, useEffect, useRef, useMemo, useCallback } from "react";
 import {Marker, Tooltip} from "react-leaflet"
 import {Icon} from "leaflet"
+import axios from 'axios';
 
 
   const customIconOff = new Icon({
@@ -24,8 +25,36 @@ import {Icon} from "leaflet"
       currentIconIndex = 1
       done = 1
     }
+    const reqDone = useRef(false);
+    //Check the DB and get the status of the markers
+    const updateMarkersWithDB = () =>
+      {
+        if(!reqDone.current)
+          {
+            reqDone.current = true; //to make sure the DB call only gets made once when we initialise the marker
+                let currentMarker = 
+                  {
+                    Latitude: center["lat"],
+                    Longitude: center["lng"],
+                  } //we'll use that object to find the current marker in the DB and get its status if the marker
+                  //isn't in the DB yet, we'lle get a "DNE" response and ignore it (the marker will be false anyways)
     
-  
+                axios
+                .post("http://localhost:8082/api/getmarker", currentMarker)
+                .then( (res) => 
+                  { 
+                    if (res.data== true) //if the marker is in the DB and its flag is set to true
+                      {
+                        console.log(res.data)
+                        changeIcon()
+                        currentIconIndex = 1
+                      }
+                  })
+                  .catch( (err) => { console.log(err) });   
+          }      
+      }
+    updateMarkersWithDB();
+
     const [iconStatus, setIconStatus] = useState(status)
     //const [icon, setIcon] = useState( {iconUrl: require("../icons/location.png"), iconSize: [38,38]});
     const markerRef = useRef(null)
@@ -34,12 +63,34 @@ import {Icon} from "leaflet"
         click() {
           changeIcon()
           currentIconIndex = (currentIconIndex+1)%2
+          console.log(currentIconIndex);
+          sendToDB();
         },
 
       }),
       [],
     )
     
+    // This function gets the lat and lng of the current marker as well as its status and updates 
+    // (or adds if it doesn't already exist) the marker in the DB 
+    const sendToDB = () =>
+      {
+        let currentMarker = 
+          {
+            Latitude: center["lat"],
+            Longitude: center["lng"],
+            Status: currentIconIndex
+          }
+        axios
+          .post('http://localhost:8082/api/updatemarker', currentMarker)
+          .then((res) => {
+              console.log(res)
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+
     const changeIcon = useCallback(() => {
       setIconStatus((d) => !d)
       
