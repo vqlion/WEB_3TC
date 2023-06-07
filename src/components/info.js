0c3454199033732ff022f498f1d95284b6aafb65
vql <valentin.jossic@insa-lyon.fr>
2023-06-07 11:44:29 +0200

    CSS cleaning, icons on profile page

--- src/components/info.js
+++ src/components/info.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import './info.css'
 import '../index.css'
 function Userdata() {
     return ( 
         <div className='wrapper'>
-            <div className='header'> welcome </div>
+            <header className='header'> welcome </header>
             <div className='contenu'>
                 <h1>Données de l'utilisateur</h1>
              </div>
--- src/components/userpage1.js
+++ src/components/userpage1.js
@@ -1,27 +1,47 @@
 import './user_page1.css'
 import '../index.css'
 import React from 'react'
-import { FaRegUserCircle, FaRegListAlt } from "react-icons/fa";
+import { FaRegUserCircle, FaRegListAlt, FaChevronDown, FaChevronUp, FaHome } from "react-icons/fa";
 import { Outlet } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import styles from './menu.module.css'
+import styled from 'styled-components'
+import { useState } from 'react';
 function Userpage1() {
-    const getUserInfo = () =>
-        {
-            // axios
-            //     .get('http://localhost:8082/api/getusers')
-            //     .then((res) => console.log(res))
-        }  
+    const getUserInfo = () => {
+        // axios
+        //     .get('http://localhost:8082/api/getusers')
+        //     .then((res) => console.log(res))
+    }
+
+    const FlexDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+    align-items: center;`
+
+    const [visibleStatus, setVisibleStatus] = useState(false)
+
+    const getChevron = () => {
+        return visibleStatus ? <FaChevronUp className='icon'/> : <FaChevronDown className='icon'/>
+    }
 
     return (
-        <div className='mainContainer'>
-            <div className='header'>
-                <h2 class='nameAppli'>NomAppli</h2>
-            </div>
-            <div className='wrapper'>
-                <Link className='link-box' to="info" onClick={getUserInfo}><FaRegUserCircle className='icon' />Informations</Link>
-                <Link className='link-box' to="asso"><FaRegListAlt className='icon' />Listes assos</Link>
-            </div>
+        <div className='wrapper'>
+            <header className='header'>
+                <div className={styles.menu}>
+                    <FlexDiv>
+                        <h2 className='nameAppli'>Username</h2>
+                        <button onClick={() => setVisibleStatus(!visibleStatus)}> {getChevron()}</button>
+                    </FlexDiv>
+                    <nav className='links-container' data-visible={visibleStatus}>
+                        <Link className='link-box' to="/"><FaHome className='icon' /></Link>
+                        <Link className='link-box' to="info" onClick={getUserInfo}><FaRegUserCircle className='icon' /></Link>
+                        <Link className='link-box' to="asso"><FaRegListAlt className='icon' /></Link>
+                    </nav>
+                </div>
+            </header>
             <Outlet />
         </div>
 
