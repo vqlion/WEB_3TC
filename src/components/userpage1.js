6f3885abc8fe23fc186853fb0b09093316bba227
alicelbh <alice@cogibulle.com>
2023-06-07 09:25:27 +0200

    Rest of the commit

--- src/components/userpage1.js
+++ src/components/userpage1.js
@@ -1,17 +1,32 @@
-import './user_page1.css' 
+import './user_page1.css'
+import '../index.css'
 import React from 'react'
-// import Group from './Group'
+import { FaRegUserCircle, FaRegListAlt } from "react-icons/fa";
+import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 function Userpage1() {
-    return ( 
+    const getUserInfo = () =>
+        {
+            axios
+                .get('http://localhost:8082/api/getusers')
+                .then((res) => console.log(res))
+        }  
+
+    return (
         <div className='mainContainer'>
-            <div className='header'> Header </div>
-            <div className='contenu'>
-                <a href="info">information</a>
-                <a href="asso">Listes Assos</a>
-             </div>
+            <div className='header'>
+                <h2 class='nameAppli'>NomAppli</h2>
+            </div>
+            <div className='wrapper'>
+                <Link className='link-box' to="info" onClick={getUserInfo}><FaRegUserCircle className='icon' />Informations</Link>
+                <Link className='link-box' to="asso"><FaRegListAlt className='icon' />Listes assos</Link>
+            </div>
+            <Outlet />
         </div>
 
-     );
+    );
+
 }
 
 export default Userpage1;
