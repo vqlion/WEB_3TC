53751ac366d77be23a974d46ea62cb4e25b252aa
alicelbh <alice@cogibulle.com>
2023-06-15 09:15:30 +0200

    get list of asso for the map

--- src/components/AssoList.js
+++ src/components/AssoList.js
@@ -1,21 +1,36 @@
 import styles from './assoList.module.css'
-import { useState } from 'react'
-
+import { useState, useRef } from 'react'
+import axios from "axios"
 export function AssoList() {
 
     const [clickedButton, setClickedButton] = useState()
     const [buttons, setButtons] = useState([])
 
-    const buttonProps = [{
-        id: 0,
-        text: 'Exit'
-    }, {
-        id: 1,
-        text: 'Karna'
-    }, {
-        id: 2,
-        text: 'Lezarts'
-    }]
+    var userId = "6489fcfb22ecc34e945ab951"
+    const [listasso, updatelistasso] = useState([]);
+    const reqDone = useRef(false);
+    function getUserInfo()
+        {
+            reqDone.current=true;
+            axios
+                .post("http://localhost:8082/api/getusers", {"google_id": userId})
+                .then(res=>
+                        {
+                            let asso = res.data[0]["assoList"];
+                            updatelistasso(asso);
+                        })
+                .catch(err => console.log(err))
+        }
+    if (reqDone.current==false) getUserInfo()
+
+    console.log(listasso)
+    let i=0
+    let buttonProps = listasso.map((a) =>
+        {
+            let temp = {"id": i, "text": a}
+            i++
+            return (temp)
+        })
 
     const buttonGenerator = (props) => {
         return <button onClick={handleButtonClick} data-id={props.id} data-clicked={clickedButton == props.id ? true : false}>{props.text}</button>
